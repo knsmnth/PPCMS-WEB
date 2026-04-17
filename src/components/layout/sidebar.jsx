e14e766e40b982e4c57df87a7ff8401c06a536b7
Alvin Macion <macion.et.al@gmail.com>
2026-04-17 21:58:39 +0800

    feat: hides api intergration in sidebar

--- src/components/layout/sidebar.jsx
+++ src/components/layout/sidebar.jsx
@@ -169,14 +169,14 @@ export function Sidebar({ className }) {
                   <ShieldAlert className={styles.icon} />
                   <span className={styles.linkText}>Access Management</span>
                 </NavLink>
-                <NavLink
+                {/* <NavLink
                   to="/api-integrations"
                   className={({ isActive }) => clsx(styles.link, isActive && styles.link_active)}
                   title={isCollapsed ? 'API Integrations' : ''}
                 >
                   <Network className={styles.icon} />
                   <span className={styles.linkText}>API Integrations</span>
-                </NavLink>
+                </NavLink> */}
                 <NavLink
                   to="/data-management"
                   className={({ isActive }) => clsx(styles.link, isActive && styles.link_active)}
